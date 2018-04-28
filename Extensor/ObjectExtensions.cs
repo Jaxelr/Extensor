c496b8784f7418b0537ddba986a2c6463f87e2c7
Joel Roman Sosa <joelrsosarivera@gmail.com>
2018-04-28 19:56:41 -0400

    Short and Object extenions with their unit tests

---
+++ Extensor/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Extensor
+{
+    /// <summary>
+    /// Returns true if the object reference is null
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object o) => o == null;
+    }
+}
---
+++ Extensor/ShortExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensor
+{
+    public static class ShortExtensions
+    {
+        public static bool In(this short v, params short[] values) => values.Contains(v);
+
+        public static bool In(this short v, IEnumerable<short> values) => values.Contains(v);
+    }
+}
